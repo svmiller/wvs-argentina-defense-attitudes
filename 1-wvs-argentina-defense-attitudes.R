856042065362197d5a1809e1488d686f2239691e
steve <steven.v.miller@gmail.com>
2014-11-10 14:56:00 -0500

    Update 1-wvs-argentina-defense-attitudes.R

--- 1-wvs-argentina-defense-attitudes.R
+++ 1-wvs-argentina-defense-attitudes.R
@@ -20,13 +20,14 @@ Data$z.respectauthority <- with(Data, (respectauthority - mean(respectauthority,
 # Compare coefficient magnitudes.
 
 M2 <- glm(aims.defense ~ age + female + unemployed + z.satisfinancial + z.postma4 + z.respectauthority + nationalpride, data=Data, family=binomial(link="logit"))
+summary(M2)
 
 # Zelig sims don't play well with missing data. Let's just subset what we want.
 
 Data2 <- with(Data,  data.frame(aims.defense, age, female, unemployed, satisfinancial, postma4, respectauthority, nationalpride))
 Data2 <- na.omit(Data2)
 
-M2 <- zelig(aims.defense ~ age + female + unemployed + satisfinancial + postma4 + respectauthority + nationalpride, model = "logit", data = Data2)
+M3 <- zelig(aims.defense ~ age + female + unemployed + satisfinancial + postma4 + respectauthority + nationalpride, model = "logit", data = Data2)
 
 M3.low <- setx(M3, respectauthority = -1, satisfinancial = 1:10)
 M3.high <- setx(M3, respectauthority = 1, satisfinancial = 1:10)
